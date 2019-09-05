a7afb7db9192613ca6b8f427afcb5d3194503848
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2019-09-05 12:02:05 -0400

    brand new t minus 6 hours

---
+++ create-react-express/client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import SearchArea from "./components/SearchArea";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <SearchArea />
+    );
+  }
+}
+
+export default App;
---
+++ create-react-express/client/src/components/SearchArea.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import "../App.css";
+
+class SearchArea extends Component {
+    render() {
+        return (
+            <div>
+
+                <h1>Jonny O</h1>
+            </div>
+        );
+    }
+}
+
+export default SearchArea;
